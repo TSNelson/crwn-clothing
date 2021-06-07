3aacc1db215e042e5911840e1102ad8884ec342b
Tor Nelson <TSNelson@users.noreply.github.com>
2021-06-07 01:56:55 -0700

    split homepage into components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 // Components
-import HomePage from './components/homepage/homepage';
+import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
