04ac87d419082893ba961a61a30e7cfedadc9cb0
Tor Nelson <TSNelson@users.noreply.github.com>
2021-08-08 02:34:10 -0700

    Implemented cart toggle action

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { auth } from '../../firebase/firebase.utils';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
+
 import './header.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (<div className='header'>
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
+
     <div className='options'>
       <Link className='option' to='/shop'>SHOP</Link>
       <Link className='option' to='/contact'>CONTACT</Link>
@@ -18,13 +23,16 @@ const Header = ({ currentUser }) => {
         <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
         <Link className='option' to='/sign-in'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
+
+    { hidden ? null : <CartDropdown /> }
 
   </div>);
 }
 
-const mapStateToProps = state => {
-  return { currentUser: state.user.currentUser }
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => {
+  return { currentUser, hidden }
 }
 // connect is a higher order component that passes props into another component from redux
 export default connect(mapStateToProps)(Header);
